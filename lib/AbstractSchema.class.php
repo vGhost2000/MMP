b10ac3c2c97981182c0095d68d55aa7395911af3
vGhost <vGhost2000@gmail.com>
2017-11-30 00:49:48 +0400

    немного защиты от сбоев при миграциях

--- lib/AbstractSchema.class.php
+++ lib/AbstractSchema.class.php
@@ -4,7 +4,25 @@ abstract class AbstractSchema
 {
     public function load(Mysqli $db)
     {
+        $drop = [];
+        $other = [];
+
         foreach ($this->buildQueries() as $query) {
+            $query = trim($query);
+            if (preg_match('#^drop#i', $query)) {
+                $drop[] = $query;
+            } else {
+                $other[] = $query;
+            }
+        }
+        $drop = array_reverse($drop);
+        foreach ($drop as $query) {
+            Output::verbose($query);
+            if (!$db->query($query)) {
+                throw new Exception("Fail\n{$query}\n{$db->error}");
+            }
+        }
+        foreach ($other as $query) {
             Output::verbose($query);
             if (!$db->query($query)) {
                 throw new Exception("Fail\n{$query}\n{$db->error}");
